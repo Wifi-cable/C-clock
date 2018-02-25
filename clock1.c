42052d0764ad1ce849c023265fed676210e3dba0
wifi-cable <Wifi-cable@users.noreply.github.com>
2018-02-25 14:29:11 +0100

    buttons wired wrong

--- clock1.c
+++ clock1.c
@@ -2,16 +2,18 @@
 #include <avr/interrupt.h> 	// intterupts instead of delay
 
 int main(void);
+uint8_t minuteShifter(uint8_t in);
+
 uint8_t second=0;
 uint8_t minute=0;
-//uint8_t hour=0;	// so i can see if the LEDs turn on
+uint8_t hour=11;	// so i can see if the LEDs turn on
 volatile uint8_t flag; //stop and check if a second is over
 uint8_t softtimer;
 int setMinute;
 int setHour;
 uint8_t timer;
 uint8_t buttonTime;
-uint8_t hourArray[12] = {0,8,4,12,2,10,6,14,1,9,5,13};
+//uint8_t hourArray[12] = {0,8,4,12,2,10,6,14,1,9,5,13};
 
    ISR(TIMER0_COMP_vect) {// 125.00 times per second interrupt service routine.
      flag=1;	// flag = true;
@@ -21,11 +23,11 @@ int main (void){
 	PORTA= 0;	// portA digital
 	PORTC= 0;	//portb digital
 	PORTD= 0;
-	PORTB= 0x03;
+	PORTB= 0x03;	// push buttons
 		
 	DDRA= 0xff;	//output :seconds
-	DDRC= 0xff;	//otput :mintes
-	DDRD= 0xff;	//output	:hours
+	DDRC= 0xff;	//otput :(mintes) is now hour
+	DDRD= 0xff;	//output	:(hours) is now minute needs helper method
 		
 	TCCR0 = 0x0b;	// pre devisor
 	OCR0 = 127;
@@ -44,21 +46,23 @@ int main (void){
 			
 			if(minute>=60){	//minute overflow
 				minute=0;
-				//hour++;
-				index++;
+				hour++;
+		//		index++;
 					
-//				if(hour>=12){		//hour overflow
-//					hour=0;
-//					}
-				if(index>=12){
-					index=0;
+				if(hour>=12){		//hour overflow
+					hour=0;
 					}
+//				if(index>=12){
+//					index=0;
+//					}
 				}
 			}
 			
 		PORTA=second;	//binary output
-		PORTD=minute;
-		PORTC=hourArray[index];
+		//PORTD=minute;
+		PORTD= hour;
+	//PORTC=hourArray[index];
+		PORTC=  minuteShifter(minute);
 
 	   if (flag){
         flag = 0; // divide time to get a full second
@@ -84,9 +88,9 @@ int main (void){
 			 	}
 		}
 		if(!setHour){	// same for hours. if half a second has passed and button is pressed
-			 index++;
-				if(index>=12){
-					index=0;
+			 hour++;
+				if(hour>=12){
+					hour=0;
 					}
 				}
 			}
@@ -94,3 +98,28 @@ int main (void){
 	}	//end big while true loop
 	return 1;	//unreachable code to make the C language happy
 }
+// method to make little endian out of big endian nummbers, turn on other LEDs
+uint8_t minuteShifter(uint8_t in){
+	uint8_t reverse=0;
+			if(in & 1){
+				reverse |= 32;
+	}
+			if (in & 2){
+				reverse |= 16;
+		}
+			if (in & 4){
+				reverse |= 8;
+		}
+			if (in & 8){
+				reverse |= 4;
+		}
+			if (in &16){
+				reverse |= 2;
+		}
+			if (in &32){
+				reverse |= 1;
+		}
+
+	
+	return reverse;
+	}
